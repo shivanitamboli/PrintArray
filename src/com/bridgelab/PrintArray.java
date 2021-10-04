03b47cb932df1faabadd5f0526cc1b6f987c00bb
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-10-04 08:42:43 +0530

    Added : print array using generics

--- src/com/bridgelab/PrintArray.java
+++ src/com/bridgelab/PrintArray.java
@@ -1,32 +1,19 @@
 package com.bridgelab;
 
-	public class PrintArray {
-	    public static void main(String[] args) {
-	        int[] a = {1,2,3};
-	        double[] b = {1.1,2.2,3.3};
-	        char[] c = {'a','b','c'};
-	        toPrint(a);
-	        toPrint(b);
-	        toPrint(c);
-	    }
-	    private static void toPrint(int[] a) {
-	        for(int i:a)
-	        {
-	            System.out.println(i);
-	        }
-	    }
-	    private static void toPrint(double[] b) {
-	        for(double i:b)
-	        {
-	            System.out.println(i);
-	        }
-	    }
-	    private static void toPrint(char[] c) {
-	        for(char i:c)
-	        {
-	            System.out.println(i);
-	        }
-	    }
+public class PrintArray {
+	public static void main(String[] args) {
+		Integer[] a = { 1, 2, 3 };
+		Double[] b = { 1.1, 2.2, 3.3 };
+		Character[] c = { 'a', 'b', 'c' };
+		toPrint(a);
+		toPrint(b);
+		toPrint(c);
 	}
 
+	private static <E> void toPrint(E[] a) {
+		for (E i : a) {
+			System.out.println(i);
+		}
+	}
 
+}
